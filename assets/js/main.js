37ce8570cf1581699b3bc0065294dc3b29cbedea
Victor <vitgermim@gmail.com>
2024-02-09 16:11:31 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+const countDownDate = new Date("April 5, 2024").getTime();
+
+const x = setInterval(function () {
+
+    const now = new Date().getTime();
+
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("days").textContent = days < 10 ? `0${days}` : days;
+    document.getElementById("hours").textContent = hours < 10 ? `0${hours}` : hours;
+    document.getElementById("minutes").textContent = minutes < 10 ? `0${minutes}` : minutes;
+    document.getElementById("seconds").textContent = seconds < 10 ? `0${seconds}` : seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "FELIZ ANIVERSÁRIO!!";
+    }
+}, 1000);
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("openModalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
